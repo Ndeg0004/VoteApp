ca4d8f9f8d0b9978c1358c41fc32435991dbb686
crevilla <crevilla043@gmail.com>
2024-12-16 18:53:06 -0500

    LoginActivty and SplashActivity changes

--- app/src/main/java/com/example/voteapp/SplashActivity.java
+++ app/src/main/java/com/example/voteapp/SplashActivity.java
@@ -21,10 +21,10 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
-        }, 3000);
+        }, 2000);
     }
 }
